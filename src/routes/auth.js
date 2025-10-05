1a06c259e4158c4f503565ee67c816a08f81d8d6
Shubhodeep Mukherjee <shubhodeepmukherjee24@gmail.com>
2025-10-05 21:25:52 +0530

    fix: fixed the server error message to previous in register route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -50,7 +50,7 @@ router.post("/register", validateBody(registerSchema), async (req, res) => {
 
     res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
-    res.status(500).json({ message: err });
+    res.status(500).json({ message: "Server error" });
   }
 });
 
