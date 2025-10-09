85234b27d56993bbb273f99f2e0d15eff88fe15b
Shubhodeep Mukherjee <shubhodeepmukherjee24@gmail.com>
2025-10-09 23:23:01 +0530

    fix: cross origin added for localhost 3000 frontend for development enviroment

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import authRoutes from "./routes/auth.js";
 import customerRoutes from "./routes/customers.js";
 import leadRoutes from "./routes/leads.js";
-
+import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -14,6 +14,10 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/customers", customerRoutes);
 app.use("/api/leads", leadRoutes);
+app.use(cors({
+  origin: "http://localhost:3000", // frontend URL
+  credentials: true
+}));
 
 // Connect MongoDB only in non-test env
 if (process.env.NODE_ENV !== "test") {
